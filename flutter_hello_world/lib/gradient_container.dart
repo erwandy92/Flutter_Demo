4ac4b81147910aabd0f9121690b159d4bc73f76a
Erwandy Lim <erwandy.dev@gmail.com>
2026-05-25 00:27:45 +0800

    Set decoration alignment as variables (const)

--- flutter_hello_world/lib/gradient_container.dart
+++ flutter_hello_world/lib/gradient_container.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hello_world/styled_text.dart';
 
+//Variables for gradient alignment
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget{
   const GradientContainer({super.key});
 
   @override
   Widget build(context){
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
-              const Color.fromARGB(255, 58, 145, 61),
+               Color.fromARGB(255, 58, 145, 61),
               Colors.blue
             ], 
-            begin: Alignment.topLeft, 
-            end: Alignment.bottomRight
+            begin: startAlignment, 
+            end: endAlignment
         ),
       ),
       child: const Center(
