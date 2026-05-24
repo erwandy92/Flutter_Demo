1d5dffd21c026ecb20897c2546450b76bd985da2
Erwandy Lim <erwandy.dev@gmail.com>
2026-05-25 00:01:36 +0800

    Created GradientContainer as a class for the Scaffold body

--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -2,22 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.green,
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                  const Color.fromARGB(255, 58, 145, 61),
-                  Colors.blue
-                ], 
-                begin: Alignment.topLeft, 
-                end: Alignment.bottomRight
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ), 
   );
+}
+
+class GradientContainer extends StatelessWidget{
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context){
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+              const Color.fromARGB(255, 58, 145, 61),
+              Colors.blue
+            ], 
+            begin: Alignment.topLeft, 
+            end: Alignment.bottomRight
+        ),
+      ),
+    );
+  }
 }
