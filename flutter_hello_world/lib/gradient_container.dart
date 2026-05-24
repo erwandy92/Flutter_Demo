4916d4d26dc5e327bc9e9ec0de8018d2ef3cb486
Erwandy Lim <erwandy.dev@gmail.com>
2026-05-25 00:14:56 +0800

    Make the StyledText as a new class file

--- flutter_hello_world/lib/gradient_container.dart
+++ flutter_hello_world/lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hello_world/styled_text.dart';
 
 class GradientContainer extends StatelessWidget{
   const GradientContainer({super.key});
@@ -17,13 +18,7 @@ class GradientContainer extends StatelessWidget{
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontSize: 24,
-            color: Colors.white,
-          ),
-        ),
+        child: StyledText(),
       ),
     );
   }
