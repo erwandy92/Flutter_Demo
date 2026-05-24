98aafeecdfd32d943d94f3e4154e463aff848307
Erwandy Lim <erwandy.dev@gmail.com>
2026-05-25 00:37:30 +0800

    Pass data from main to StyledText

--- flutter_hello_world/lib/styled_text.dart
+++ flutter_hello_world/lib/styled_text.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build(context) {
-    return const Text(
-      'Hello World!',
+    return Text(
+      text,
       style: TextStyle(
         fontSize: 24,
         color: Colors.white,
