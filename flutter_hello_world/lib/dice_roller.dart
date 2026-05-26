6d07bf539e21b8dcab6ca90e27337b6d216c3f17
Erwandy Lim <erwandy.dev@gmail.com>
2026-05-27 00:48:01 +0800

    Improved the randomize method

--- flutter_hello_world/lib/dice_roller.dart
+++ flutter_hello_world/lib/dice_roller.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+final randomizer = Random(); //to avoid creating a new instance of Random every time we roll the dice, we can create a single instance and reuse it. This is more efficient and also allows us to maintain the state of the random number generator if needed.
+
+//Extend StatefulWidget instead of StatelessWidget because we need to maintain the state of the dice roll and update the UI accordingly.
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
@@ -10,14 +13,14 @@ class DiceRoller extends StatefulWidget {
   }
 }
 
+// As we use StatefulWidget, we need to create a State class that will hold the state of the widget and handle the logic for rolling the dice and updating the UI. The State class is private (indicated by the underscore) because it should only be used within this file and not exposed to other parts of the application.
 // underscore means that the class is private and can only be used within this file. It is a common convention in Dart to prefix private classes with an underscore.
 class _DiceRollerState extends State<DiceRoller>{
-  var activeDiceImage = 'assets/images/dice-1.png';
+  var currentDiceRoll = 2;
 
   void rollDice() {
-    var rolledNumber = Random().nextInt(6) + 1; // Generate a random number between 1 and 6
     setState(() {
-      activeDiceImage = 'assets/images/dice-$rolledNumber.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1; // Generate a random number between 1 and 6
     });
     // activeDiceImage = 'assets/images/dice-2.png';
   }
@@ -28,8 +31,8 @@ class _DiceRollerState extends State<DiceRoller>{
           mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
-              activeDiceImage, 
-              width: 200, 
+              'assets/images/dice-$currentDiceRoll.png',
+              width: 200,
               height: 200,
             ),
             const SizedBox(height: 30), // Add some spacing between the image and the button
--- flutter_hello_world/lib/main.dart
+++ flutter_hello_world/lib/main.dart
@@ -6,7 +6,12 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.green,
-        body: GradientContainer.blue(),
+        // body: GradientContainer.blue(),
+        // Set the body of the Scaffold to be an instance of our GradientContainer widget, passing in the desired start and end colors for the gradient.
+        body: GradientContainer(
+          Color.fromARGB(255, 4, 41, 100), 
+          Colors.blue
+        ),
       ),
     ), 
   );
